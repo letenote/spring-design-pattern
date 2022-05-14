d48775b236af54995896174a43280d46b4604c58
aditReza <lete.note3@gmail.com>
2022-05-15 01:37:24 +0700

    add prototype object design pattren implementation, test coverage 100%

--- src/main/java/letenote/designpattern/prototype/employee/EmployeeSalary.java
+++ src/main/java/letenote/designpattern/prototype/employee/EmployeeSalary.java
@@ -1,6 +1,9 @@
 package letenote.designpattern.prototype.employee;
 
 public enum EmployeeSalary {
+//	public static final Long STAFF = 7_500_000L;
+//	public static final Long MANAGER = 18_000_000L;
+//	public static final Long VP = 28_000_000L;
 	STAFF(7_500_000L),
 	MANAGER(18_000_000L),
 	VP(28_000_000L);
